05e76294154e1be84553cc15c90843c12f7b641a
Hendrik Brinkmann <hbrinkmann@timocom.com>
2017-09-05 14:09:08 +0200

    formatting

--- update.sh
+++ update.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 #
 # apt: update, upgrade and cleanup
+
 apt update
 apt dist-upgrade -y
 apt autoremove -y
 apt autoclean -y
-purge-old-kernels 
+purge-old-kernels
 update-grub
 update-grub2
