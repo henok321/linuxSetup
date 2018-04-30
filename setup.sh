f78fbde2f0be2e68c80199216137fb7061218daf
Hendrik Brinkmann <hendrik@brinkmann.one>
2018-04-30 16:02:53 +0200

    remove ppas

--- setup.sh
+++ setup.sh
@@ -12,44 +12,6 @@ if [[ "y" != "$input" ]]; then
   exit 0
 fi
 
-# add ppa's and sources
-
-# git
-add-apt-repository ppa:git-core/ppa -y
-
-# oracle jdk
-add-apt-repository ppa:webupd8team/java -y
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-
-#atom
-add-apt-repository ppa:webupd8team/atom -y
-
-# Google Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-
-# nodejs
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-
-# docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-os=$(lsb_release -cs)
-
-declare -a mint=("sonya" "serena" "sarah")
-
-for e in "${mint[@]}"
-do
-  if [[ "$e" == "$os" ]]; then
-    os="xenial"
-  fi
-done
-
-sudo add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $os \
-stable"
 
 # update
 apt update
@@ -63,10 +25,4 @@ apt install -y gitg
 apt install -y git-cola
 apt install -y kdiff3
 apt install -y vim tmux bmon htop screen
-apt install -y oracle-java8-installer oracle-java8-unlimited-jce-policy oracle-java8-set-default
-apt install -y nodejs
 apt install -y apt-transport-https ca-certificates curl software-properties-common
-apt install -y docker-ce docker-compose
-apt install -y atom
-apt install -y google-chrome-stable
-apt install -y keychain
